a5b2b5e1ecdea63c053aa7c29e1b4706b36cd78b
JulienTouchard <julien.touchard@free.fr>
2023-12-14 21:30:01 +0100

    add qte for "Achat Immediat" btn

--- components/CardItem/CardItem.js
+++ components/CardItem/CardItem.js
@@ -4,15 +4,16 @@ import BoutiqueContext from '../../BoutiqueContext';
 const CardItem = (props) => {
     const boutiqueContext = React.useContext(BoutiqueContext);
     const article = boutiqueContext.articlesApp[props.id];
-
     return (
         <Card>
-            <Card.Title title="Card Title" subtitle="Card Subtitle"  />
+            <Card.Title title={article.name} subtitle={article.price+" €"}  />
             <Card.Content>
-                <Text variant="titleLarge">{article.name}</Text>
+                <Text variant="titleLarge">{article.qte} articles restants</Text>
                 <Text variant="bodyMedium">{article.description}</Text>
             </Card.Content>
-            <Card.Cover source={article.url} />
+            <Card.Cover source={article.url} resizeMode={`cover`}
+            style={{flexDirection: 'column',height:300,width:150}}
+            />
             <Card.Actions>
                 <Button>Achat Immediat</Button>
             </Card.Actions>
